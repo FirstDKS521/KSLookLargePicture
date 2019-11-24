5c52112a31dd12551983199e39566455f7fb29de
FirstDKS521 <870815325@qq.com>
2019-11-24 18:03:12 +0800

    Initial Commit

---
+++ KSLookLargePicture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KSLookLargePicture
+//
+//  Created by aDu on 2019/11/24.
+//  Copyright © 2019 aDu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KSLookLargePicture/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KSLookLargePicture
+//
+//  Created by aDu on 2019/11/24.
+//  Copyright © 2019 aDu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
